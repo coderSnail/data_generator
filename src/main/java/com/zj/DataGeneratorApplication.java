b0268d9054697be41bc5cb3a5e2bd4d6a448a1c3
SnailZJ <8351125+snailzj@user.noreply.gitee.com>
2021-01-04 17:03:16 +0800

    基础页面搭建

--- src/main/java/com/zj/DataGeneratorApplication.java
+++ src/main/java/com/zj/DataGeneratorApplication.java
@@ -3,6 +3,9 @@ package com.zj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author CoderSnail
+ */
 @SpringBootApplication
 public class DataGeneratorApplication {
 
